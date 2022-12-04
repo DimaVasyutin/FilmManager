5b789985e27c0882ad4567259de88f0bdfd69c10
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-12-04 13:39:17 +0300

    Update

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -1,30 +1,44 @@
 public class PosterManager {
-    private String[] movies = new String[0];
+    private RepositoryPosterManager repo;
     private int limit;
 
     public PosterManager() {
         this.limit = 10;
     }
 
-    public PosterManager(int limit) {
+
+    public PosterManager(RepositoryPosterManager repo, int limit) {
+        this.repo = repo;
         this.limit = limit;
     }
 
-
-    public void addFilm(String movie) {
-        String[] tmp = new String[movies.length + 1];
-        for (int i = 0; i < movies.length; i++) {
-            tmp[i] = movies[i];
-        }
-        tmp[tmp.length - 1] = movie;
-        movies = tmp;
+    public void addFilm(Poster movie) {
+        repo.addFilm(movie);
     }
 
-    public String[] findAll() {
-        return movies;
+    public Poster[] removeById(int id) {
+        Poster[] tmp = repo.removeById(id);
+        return tmp;
     }
 
-    public String[] findLast() {
+    public Poster[] removeAll() {
+        Poster[] tmp = repo.removeAll();
+        return tmp;
+    }
+
+    public Poster[] findById(int id) {
+        Poster[] tmp = repo.findById(id);
+        return tmp;
+    }
+
+    public Poster[] findAll() {
+        Poster[] tmp = repo.findAll();
+        return tmp;
+    }
+
+
+    public Poster[] findLast() {
+        Poster[] movies = repo.findAll();
         int limitLenght;
 
         if (movies.length < limit) {
@@ -33,7 +47,7 @@ public class PosterManager {
             limitLenght = limit;
         }
 
-        String[] tmp = new String[limitLenght];
+        Poster[] tmp = new Poster[limitLenght];
 
         for (int i = 0; i < limitLenght; i++) {
             tmp[i] = movies[movies.length - 1 - i];
